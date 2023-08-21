bf422de149829da3d408fa5a031d74271a5622a0
PanzyOfficial <116108560+PanzyOfficial@users.noreply.github.com>
2023-08-21 07:20:51 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let navbar = document.querySelector('.header .navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.add('active');
-     dropdownContent.style.color = 'black';
+    
 }
 
 document.querySelector('#nav-close').onclick = () =>{
