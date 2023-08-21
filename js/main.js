fca4c4f988d1ab1295ea8744fcf7ebf5fdf65795
PanzyOfficial <116108560+PanzyOfficial@users.noreply.github.com>
2023-08-21 07:04:05 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.add('active');
+}
+
+document.querySelector('#nav-close').onclick = () =>{
+    navbar.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.add('active');
+}
+
+document.querySelector('#close-search').onclick = () =>{
+    searchForm.classList.remove('active');
+}
 var swiper = new Swiper(".Shows", {
     spaceBetween: 30,
     centeredSlides: true,
